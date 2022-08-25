4c430e25be47911134c360913c86441d5d16b5e4
Gheraldine Salazar <gh.eeraldin@gmai.com>
2022-08-24 23:04:33 -0300

    Landing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import logo from '../img/logo.png'
+import logo from '../assets/logo.png'
 
 export const Navbar = () => {
     return(
@@ -9,27 +9,13 @@ export const Navbar = () => {
                 </a>
 
                 <ul>
-                    <li>
-                        <a href="#">LIVING</a>
-                    </li>
-                    <li>
-                        <a href="#">COMEDOR</a>
-                    </li>
-                    <li>
-                        <a href="#">OFFICE / ESTUDIO</a>
-                    </li>
-                    <li>
-                        <a href="#">HABITACIÓN</a>
-                    </li>
-                    <li>
-                        <a href="#">COCINA</a>
-                    </li>
-                    <li>
-                        <a href="#">BAÑO</a>
-                    </li>
-                    <li>
-                        <a className='sale' href="#">Sale</a>
-                    </li>
+                    <li> <a href="#">LIVING</a> </li>
+                    <li> <a href="#">COMEDOR</a> </li>
+                    <li> <a href="#">OFFICE / ESTUDIO</a> </li>
+                    <li> <a href="#">DORMITORIO</a> </li>
+                    <li> <a href="#">COCINA</a> </li>
+                    <li> <a href="#">BAÑO</a> </li>
+                    <li> <a className='sale' href="#">Sale</a> </li>
                 </ul>
 
             </nav>
